3367124893c75c3f99318ed4bb868d4f48ddc0bd
Ozan Kenan Güngör <ozankenangungor@Ozan-MacBook-Pro.local>
2025-08-25 00:59:46 +0300

    Fix the initialization

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,21 @@ use prompts::{Prompt, text::TextPrompt};
 
 #[tokio::main]
 async fn main() {
-    let mut prompt = TextPrompt::new("$ ");
+    let mut prompt = TextPrompt::new("");
 
-    match prompt.run().await {
-        Ok(Some(input)) => {
-            println!("{input}")
+    loop {
+        match prompt.run().await {
+            Ok(Some(input)) => {
+                if (input == "q") {
+                    break;
+                }
+                println!("{input}")
+            }
+            Err(error) => {
+                println!("Error: {error}");
+            }
+            _ => {}
         }
-        Err(error) => {
-            println!("Error: {error}");
-        }
-        _ => {}
+        prompt = TextPrompt::new("");
     }
 }
