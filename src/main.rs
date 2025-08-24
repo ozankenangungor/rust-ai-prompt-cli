26634a6022daf6774cc421be0f5cd6e428f41ab2
Ozan Kenan Güngör <ozankenangungor@Ozan-MacBook-Pro.local>
2025-08-25 00:45:35 +0300

    initialize prompt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use prompts::{Prompt, text::TextPrompt};
+
+#[tokio::main]
+async fn main() {
+    let mut prompt = TextPrompt::new("$ ");
+
+    match prompt.run().await {
+        Ok(Some(input)) => {
+            println!("{input}")
+        }
+        Err(error) => {
+            println!("Error: {error}");
+        }
+        _ => {}
+    }
 }
